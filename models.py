8d346197f278c146871a62f503e371297ffcf08a
Nauf <130215373+naufudge@users.noreply.github.com>
2024-11-03 23:51:58 +0500

    fixed a typo

--- models.py
+++ models.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Boolean, Column, Integer, String
 from database import Base
 
-class User(Base):
+class Users(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
