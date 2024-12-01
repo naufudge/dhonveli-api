ea34435b16a211cdb4c1b14256be41df01bb7d42
Nauf <130215373+naufudge@users.noreply.github.com>
2024-12-01 21:44:58 +0500

    removed NoneType from room_type

--- schemas.py
+++ schemas.py
@@ -40,7 +40,7 @@ class HotelRoom(BaseModel):
     booking_id: int | None = None
     booking: Any | None = None
     room_type_id: int | None = None
-    room_type: HotelRoomType | None = None
+    room_type: HotelRoomType
 
 class CreateHotelRoom(BaseModel):
     hotel_room: HotelRoom
