4d09d523caeeebfafb7833c699124f584207a45b
Nauf <130215373+naufudge@users.noreply.github.com>
2024-12-25 23:46:23 +0500

    fixed module missing error

--- database.py
+++ database.py
@@ -1,12 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv
+import pymysql
 import os
 
 load_dotenv()
 
 URL = os.getenv("DB_URL")
 
+pymysql.install_as_MySQLdb()
 engine = create_engine(URL)
 
 SessionLocal = sessionmaker(autocommit = False, autoflush= False, bind=engine)
